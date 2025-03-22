486c1d2c73faae1b71a9dd0e9da889e9d7e6d8f4
xsodus <apocalyps26@gmail.com>
2025-03-22 20:38:18 +0700

    Update printHello function to accept a name parameter and enhance logging output

--- src/index.js
+++ src/index.js
@@ -10,8 +10,9 @@ try {
   core.setOutput("time", time);
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2)
-  printHello();
-  console.log(`The event payload: ${payload}`);
+  // Call a simple function from another file
+  printHello(nameToGreet);
+  console.log(`############### The event payload ##################### \n ${payload}`);
 } catch (error) {
   core.setFailed(error.message);
 }
--- src/testFunction.js
+++ src/testFunction.js
@@ -1,3 +1,3 @@
-export function printHello() {
-  console.log("Hello, world!");
+export function printHello(name) {
+  console.log(`Hello, ${name}`);
 }
